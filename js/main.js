252c0338a729a5c5502958286b2f96bab276f028
zatzoid <zatzoid@gmail.com>
2025-01-29 12:36:46 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,536 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 382:
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+
+// EXTERNAL MODULE: ./node_modules/jquery/dist/jquery.js
+var jquery = __webpack_require__(755);
+var jquery_default = /*#__PURE__*/__webpack_require__.n(jquery);
+// EXTERNAL MODULE: ./node_modules/inputmask/dist/inputmask.js
+var inputmask = __webpack_require__(488);
+var inputmask_default = /*#__PURE__*/__webpack_require__.n(inputmask);
+// EXTERNAL MODULE: ./node_modules/@fancyapps/ui/dist/index.esm.js
+var index_esm = __webpack_require__(252);
+// EXTERNAL MODULE: ./node_modules/nouislider/dist/nouislider.mjs
+var nouislider = __webpack_require__(122);
+;// CONCATENATED MODULE: ./src/js/utils/Form.js
+class Form {
+  /**
+   * 
+   * @param {Element} formDomEl 
+   * @param {Function} submitFoo 
+   * 
+   * 
+   * 
+   */
+  constructor(formDomEl, submitFoo) {
+    this._form = formDomEl;
+    this.submitForm = submitFoo ? submitFoo : () => {};
+    this._form.setAttribute('novalidate', true);
+    this._inputContainerSelector = 'form-input-container';
+    this._inputErrorMsgSelector = 'form-input-error';
+    this._inputErrorSelector = '_error';
+    this._inputs = () => {
+      console.log(this._form.querySelectorAll('input, textarea'));
+      return this._form.querySelectorAll('input, textarea');
+    };
+    this._btnSubmit = this._form.querySelector('.form-submit');
+    this._btnSubmit.setAttribute('disabled', true);
+    this._btnSubmit.setAttribute('type', 'button');
+    this._cfg = {
+      default: {
+        name: {
+          regex: /^[A-Za-zА-Яа-яЁё ]+$/,
+          errMsg: 'Допустим ввод только букв'
+        },
+        email: {
+          regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+          errMsg: 'Поле должно быть в формате email@domain.com'
+        },
+        phone: {
+          regex: /^\+(7|375) \(\d{3}\) \d{3}-\d{2}-\d{2}$/,
+          errMsg: 'Формат номера телефона +7 (888) 888-88-88'
+        },
+        password: {
+          regex: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+          errMsg: 'Пароль должен содержать не менее 8 символов'
+        }
+      }
+    };
+    this.initForm();
+  }
+  _inputHandler(input) {
+    /**
+     * @param {input} inputDom
+     * возвращает булевое
+     */
+
+    this._btnSubmit.removeAttribute('disabled');
+    return this._validation(input);
+  }
+  _validation(input) {
+    /**
+    * @param {input} inputDom
+    * возвращает булевое
+    */
+    // валидация инпутов
+    // если у инпута (именно у инпута) есть pattern, провряет по этой реге
+    // ну как паттерн типа
+    // а если у инпута есть data-form_error_message, то и выдаст нужное сообщение
+
+    switch (input.name) {
+      case 'name':
+        return this._checkInputValid(input, input.hasAttribute('pattern') ? new RegExp(input.getAttribute('pattern')) : this._cfg.default.name.regex, input.dataset.form_error_message ? input.dataset.form_error_message : this._cfg.default.name.errMsg);
+      case 'email':
+        return this._checkInputValid(input, input.hasAttribute('pattern') ? new RegExp(input.getAttribute('pattern')) : this._cfg.default.email.regex, input.dataset.form_error_message ? input.dataset.form_error_message : this._cfg.default.email.errMsg);
+      case 'phone':
+        return this._checkInputValid(input, input.hasAttribute('pattern') ? new RegExp(input.getAttribute('pattern')) : this._cfg.default.phone.regex, input.dataset.form_error_message ? input.dataset.form_error_message : this._cfg.default.phone.errMsg);
+      case 'password':
+        return this._checkInputValid(input, input.hasAttribute('pattern') ? new RegExp(input.getAttribute('pattern')) : this._cfg.default.password.regex, input.dataset.form_error_message ? input.dataset.form_error_message : this._cfg.default.password.errMsg);
+      default:
+        //если инпут не известен, вернет true, и похуй
+        return true;
+    }
+  }
+  _checkInputValid(input, regex) {
+    let errMsg = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'че то не так написал, исправляй';
+    /**
+       * @param {input} inputDom
+       * @param {regex} regExp
+       * @param {errMsg} string
+       * возвращает булевое
+       */
+    if (!input.hasAttribute('required')) return true;
+    const inputContainer = input.closest('.' + this._inputContainerSelector),
+      errorMsg = inputContainer.querySelector('.' + this._inputErrorMsgSelector),
+      value = input.value.trim();
+    if (value.length <= 0) {
+      inputContainer.classList.add(this._inputErrorSelector);
+      errorMsg.textContent = 'Это поле обязательно.';
+      return false;
+    }
+    if (input.hasAttribute('minlength') && value.length < Number(input.getAttribute('minlength'))) {
+      inputContainer.classList.add(this._inputErrorSelector);
+      errorMsg.textContent = 'Строка должна быть не короче ' + input.getAttribute('minlength').toString() + ' символов' + '.';
+      return false;
+    }
+    if (!regex.test(value)) {
+      inputContainer.classList.add(this._inputErrorSelector);
+      errorMsg.textContent = errMsg;
+      return false;
+    }
+    inputContainer.classList.remove(this._inputErrorSelector);
+    errorMsg.textContent = '';
+    return true;
+  }
+  _onSubmit() {
+    let whatsUp = true;
+    console.log('hello');
+    for (const inp of this._inputs()) {
+      if (!this._inputHandler(inp)) {
+        whatsUp = false;
+        return;
+      }
+    }
+    if (!whatsUp) return;
+    //сабмит
+    console.log('submit');
+    this._form.dispatchEvent(new Event('submit'));
+  }
+  _fileHandler(evt) {
+    //не юзается
+    const inputContainer = evt.target.closest('.' + this._inputContainerSelector);
+    if (evt.target.value) {
+      inputContainer.classList.add('_active');
+      inputContainer.querySelector('.input-file-got').querySelector('.input-file-text').textContent = evt.target.value.split('\\').slice(-1);
+    } else {
+      inputContainer.classList.remove('_active');
+    }
+  }
+  _passowrHide() {
+    //не юзается
+    if (this._passwordInput.type == 'text') {
+      this._passwordInput.setAttribute('type', 'password');
+      this._passwordRepeatInput.setAttribute('type', 'password');
+    } else {
+      this._passwordInput.setAttribute('type', 'text');
+      this._passwordRepeatInput.setAttribute('type', 'text');
+    }
+  }
+  initForm() {
+    this._form.noValidate = true;
+    this._btnSubmit.addEventListener('click', e => {
+      this._onSubmit(e);
+    });
+    this._form.addEventListener('input', ev => {
+      this._inputHandler(ev.target);
+    });
+    this._form.addEventListener('blur', ev => {
+      this._inputHandler(ev.target);
+    });
+    this._form.addEventListener('change', ev => {
+      this._inputHandler(ev.target);
+    });
+  }
+}
+;// CONCATENATED MODULE: ./src/js/main.js
+/* node_modules */
+
+//import Swiper from 'swiper';
+//import { Navigation, Pagination, Grid, Autoplay } from 'swiper/modules';
+
+
+
+//import WOW from 'wow.js';
+//import gsap from 'gsap';
+//import { ScrollTrigger } from 'gsap/src/ScrollTrigger';
+//import CSSRulePlugin from 'gsap/all';
+/* node_modules */
+
+/* local */
+
+const HTML = document.querySelector('html'),
+  HTML_LOCK_SELECTOR = '_lock',
+  HTML_PAGELOAD_SELECTOR = '_page-loaded',
+  EV_INPUT = new Event('input', {
+    bubbles: true
+  }),
+  EV_CLICK = new Event('click', {
+    bubbles: true
+  });
+jquery_default()(function () {
+  initForms();
+  main_nouislider();
+  HTML.classList.add(HTML_PAGELOAD_SELECTOR);
+  header();
+  modalsHandler();
+  initFancybox();
+  document.addEventListener('click', ev => {
+    const {
+      classList
+    } = ev.target;
+    if (classList.contains('dd-target')) {
+      /**
+       * дроп даун
+       * переключает ._opened на своем родительском .dd-container
+       * если есть data-dd_html_lock='y' то еще и залочит html
+       */
+      ev.preventDefault();
+      const parent = ev.target.closest('.dd-container');
+      if (parent.classList.contains('_opened')) {
+        parent.classList.remove('_opened');
+        if (ev.target.dataset.dd_html_lock) {
+          HTML.classList.remove(HTML_LOCK_SELECTOR);
+        }
+      } else {
+        parent.classList.add('_opened');
+        if (ev.target.dataset.dd_html_lock) {
+          HTML.classList.add(HTML_LOCK_SELECTOR);
+        }
+      }
+    } else if (classList.contains('select-value')) {
+      /**
+       * записывает value в инут в дропдауне который select
+       * и закрывает дропдаун
+       */
+      ev.preventDefault();
+      const input = ev.target.closest('.dd-container').querySelector('.select-input');
+      if (!input || !ev.target.value) {
+        console.error('.select-input не найден, или нету значения');
+      }
+      input.value = ev.target.textContent.trim();
+      input.dispatchEvent(EV_INPUT);
+      setTimeout(() => {
+        input.dispatchEvent(EV_CLICK);
+      }, 200);
+      const prev = ev.target.closest('.dd-container').querySelectorAll('._checked');
+      if (prev) {
+        prev.forEach(e => {
+          e.classList.remove('_checked');
+        });
+      }
+      ev.target.classList.add('_checked');
+    }
+  });
+});
+function initFancybox() {
+  const anytarget = document.querySelector('[data-fancybox]');
+  if (!anytarget) return;
+  index_esm/* Fancybox */.KR.bind('[data-fancybox]', {
+    Thumbs: false,
+    //картинки снизу
+    Toolbar: {
+      display: {
+        left: ["zoom"],
+        middle: ["caption", "infobar"],
+        right: ["close"]
+      }
+    }
+  });
+}
+function initForms() {
+  const forms = document.querySelectorAll('.form');
+  if (forms) {
+    forms.forEach(e => {
+      new Form(e);
+      const phone = jquery_default()(e).find('input[name="phone"]');
+      if (phone) {
+        new (inputmask_default())('+7 (999) 999-99-99').mask(phone);
+      }
+    });
+  }
+}
+function modalsHandler() {
+  const modalOpeners = jquery_default()('[data-modal]'),
+    modalClosers = jquery_default()('.modal-closer'),
+    html = jquery_default()('html');
+  if (!modalOpeners || !modalClosers) return;
+  modalOpeners.on('click', ev => {
+    const {
+      modal,
+      submit_type
+    } = ev.currentTarget.dataset;
+    jquery_default()(`.modal-${modal}`).fadeIn().addClass('_opened').attr('data-submit_type', submit_type);
+    html.addClass('_lock');
+  });
+  modalClosers.on('click', ev => {
+    const {
+      classList
+    } = ev.target;
+    if (!classList.contains('modal-closer')) return;
+    if (classList.contains('modal')) {
+      jquery_default()(ev.target).fadeOut().removeClass('_opened');
+    } else {
+      jquery_default()(ev.target.closest('.modal')).fadeOut().removeClass('_opened');
+    }
+    html.removeClass('_lock');
+  });
+}
+function main_nouislider() {
+  const target = document.querySelector('.nouislider');
+  if (!target) return;
+
+  //это инпуты
+  const min = target.querySelector('.nouislider__min'),
+    max = target.querySelector('.nouislider__max'),
+    place = target.querySelector('.nouislider__place');
+  if (!min || !max) return;
+  nouislider/* default.create */.ZP.create(place, {
+    start: [Number(min.getAttribute('min')), Number(max.getAttribute('max'))],
+    connect: true,
+    step: 100,
+    range: {
+      'min': Number(min.getAttribute('min')),
+      'max': Number(max.getAttribute('max'))
+    }
+  });
+  place.noUiSlider.on('update', function (values, handle) {
+    min.setAttribute('value', Math.round(values[0]));
+    max.setAttribute('value', Math.round(values[1]));
+  });
+
+  /*  min.on('input', (e) => {
+       target.noUiSlider.set([e.target.value, max.val()])
+   })
+   max.on('input', (e) => {
+       target.noUiSlider.set([min.val(), e.target.value])
+   }) */
+}
+
+function header() {
+  let prevY = 0;
+  const header = document.querySelector('.header'),
+    modal = jquery_default()('.header__modal');
+  if (!header) return;
+  document.addEventListener('scroll', ev => {
+    if (prevY > window.scrollY && window.scrollY > 100) {
+      header.classList.add('_showed');
+    } else if (prevY < window.scrollY) {
+      header.classList.remove('_showed');
+    }
+    prevY = window.scrollY;
+  });
+  header.querySelectorAll('.header-modal-opener').forEach(el => {
+    el.addEventListener('click', ev => {
+      if (!ev.target.classList.contains('header-modal-opener')) return;
+      ev.stopPropagation();
+      if (!header.classList.contains('_opened')) {
+        modal.fadeIn();
+        header.classList.add('_opened');
+        HTML.classList.add(HTML_LOCK_SELECTOR);
+      } else {
+        modal.fadeOut();
+        header.classList.remove('_opened');
+        HTML.classList.remove(HTML_LOCK_SELECTOR);
+      }
+    });
+  });
+}
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	!function() {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = function(result, chunkIds, fn, priority) {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var chunkIds = deferred[i][0];
+/******/ 				var fn = deferred[i][1];
+/******/ 				var priority = deferred[i][2];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every(function(key) { return __webpack_require__.O[key](chunkIds[j]); })) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	!function() {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			179: 0,
+/******/ 			90: 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = function(chunkId) { return installedChunks[chunkId] === 0; };
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = function(parentChunkLoadingFunction, data) {
+/******/ 			var chunkIds = data[0];
+/******/ 			var moreModules = data[1];
+/******/ 			var runtime = data[2];
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some(function(id) { return installedChunks[id] !== 0; })) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkwebpack_example"] = self["webpackChunkwebpack_example"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [90,604], function() { return __webpack_require__(382); })
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
